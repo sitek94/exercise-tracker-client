70e9c5e38ae1b48899374076f343eaa0c2831243
sitek94 <msitkowski94@gmail.com>
2021-02-17 12:39:22 +0100

    create basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,54 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="container">
       <h1>Exercise tracker</h1>
+      <nav>
+        <a href="#new-user">New user</a>
+        <a href="#add-exercise">Add exercise</a>
+        <a href="#users">All users</a>
+        <a href="#log">Exercise log</a>
+      </nav>
+
+      <main>
+        <NewUser />
+        <AddExercise />
+        <AllUsers />
+        <ExerciseLog />
+      </main>
     </div>
+  );
+}
+
+function NewUser() {
+  return (
+    <section>
+      <h2>New User</h2>
+    </section>
+  );
+}
+
+function AddExercise() {
+  return (
+    <section>
+      <h2>Add Exercise</h2>
+    </section>
+  );
+}
+
+function AllUsers() {
+  return (
+    <section>
+      <h2>All Users</h2>
+    </section>
+  );
+}
+
+function ExerciseLog() {
+  return (
+    <section>
+      <h2>Exercise Log</h2>
+    </section>
   );
 }
 
