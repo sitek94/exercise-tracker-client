9a83171080f30f4d7daea9f5635ff4f78ed7e72c
sitek94 <msitkowski94@gmail.com>
2021-02-17 11:38:31 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
 function App() {
+  React.useEffect(() => {
+    fetch('http://localhost:5000/api/exercise/users').then(res => {
+      console.log(res);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Exercise tracker</h1>
     </div>
   );
 }
