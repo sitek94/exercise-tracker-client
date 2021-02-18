90d87121e01be096893c5e2d5a08d0dc2d02cab2
sitek94 <msitkowski94@gmail.com>
2021-02-18 14:58:33 +0100

    add functions to connect with api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,34 @@
 import axios from 'axios';
+import { NewExercise, NewExerciseResponse } from 'types';
 
-const exerciseTracker = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + '/api/exercise',
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-export default exerciseTracker;
+export default api;
+
+export async function getAllUsers() {
+  try {
+    const response = await api.get('/users');
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function postAddExercise(newExercise: NewExercise) {
+  try {
+    const { userId, description, duration, date } = newExercise;
+
+    const response = await api.post('/add', {
+      userId,
+      description,
+      duration,
+      date,
+    });
+    const data: NewExerciseResponse = response.data;
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
