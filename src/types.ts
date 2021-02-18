6c1961f7192c9a6fdb5455a46788189134c5d82d
sitek94 <msitkowski94@gmail.com>
2021-02-18 14:56:08 +0100

    add more types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,28 @@ export type Status = 'idle' | 'pending' | 'error' | 'success';
 
 export type Username = string;
 export type UserId = string;
+
+export interface User {
+  _id: UserId;
+  username: Username;
+}
+export type UserList = User[];
+
+export type ExerciseDescription = string;
+export type ExerciseDuration = number;
+export type ExerciseDate = string;
+
+export interface NewExercise {
+  userId: UserId;
+  description: ExerciseDescription;
+  duration: ExerciseDuration;
+  date?: ExerciseDate;
+}
+
+export interface NewExerciseResponse {
+  _id: UserId;
+  username: Username;
+  description: ExerciseDescription;
+  duration: ExerciseDescription;
+  date: ExerciseDate;
+}
