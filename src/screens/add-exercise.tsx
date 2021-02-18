ef10e09166c7358d51ca7bccc64986f1505899f9
sitek94 <msitkowski94@gmail.com>
2021-02-18 14:59:04 +0100

    add new exercise

--- src/screens/add-exercise.tsx
+++ src/screens/add-exercise.tsx
@@ -1,16 +1,44 @@
+import * as React from 'react';
+import useAsync from 'hooks/use-async';
+import { postAddExercise } from 'api';
+import { NewExerciseResponse } from 'types';
+import { formatDate } from 'utils';
+
 export default function AddExercise() {
+  const [userId, setUserId] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [duration, setDuration] = React.useState('');
+  const [date, setDate] = React.useState('');
+
+  const { status, value, error, execute } = useAsync<NewExerciseResponse>(
+    async () =>
+      postAddExercise({
+        userId,
+        description,
+        duration: Number(duration),
+        date: date ? date : formatDate(new Date()),
+      }),
+    false,
+  );
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    execute();
+  };
+
   return (
     <section>
       <h2>Add Exercise</h2>
-      <pre>POST /api/exercise/add</pre>
-      <form action="/api/exercise/add" method="POST">
+
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="userId">User ID*</label>
           <input
             id="userId"
-            name="userId"
-            type="text"
             placeholder="fCcIsTh3b3$t"
+            value={userId}
+            onChange={e => setUserId(e.currentTarget.value)}
             required
           />
         </div>
@@ -18,8 +46,8 @@ export default function AddExercise() {
           <label htmlFor="description">Description*</label>
           <input
             id="description"
-            name="description"
-            type="text"
+            value={description}
+            onChange={e => setDescription(e.currentTarget.value)}
             placeholder="Explosive push up workout"
             required
           />
@@ -28,18 +56,37 @@ export default function AddExercise() {
           <label htmlFor="duration">Duration* (minutes)</label>
           <input
             id="duration"
-            name="duration"
-            type="text"
-            placeholder="75"
+            type="number"
+            value={duration}
+            onChange={e => setDuration(e.currentTarget.value)}
             required
           />
         </div>
         <div>
           <label htmlFor="date">Date (YYYY-MM-DD)</label>
-          <input id="date" name="date" type="text" placeholder="2021-02-05" />
+          <input
+            id="date"
+            type="date"
+            value={date}
+            onChange={e => setDate(e.currentTarget.value)}
+          />
         </div>
         <button type="submit">Submit</button>
       </form>
+      {status === 'error' && (
+        <div style={{ backgroundColor: 'red', color: 'white' }}>
+          <p>Error</p>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </div>
+      )}
+      {status === 'success' && (
+        <div>
+          <div style={{ backgroundColor: 'green', color: 'white' }}>
+            <p>Success: new user created!</p>
+          </div>
+          <pre>{JSON.stringify(value, null, 2)}</pre>
+        </div>
+      )}
     </section>
   );
 }
