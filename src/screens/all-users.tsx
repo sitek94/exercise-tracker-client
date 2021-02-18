8c5cca2c0fcc01e452c18303afa65b200c630522
sitek94 <msitkowski94@gmail.com>
2021-02-18 14:58:52 +0100

    fetch and display all users

--- src/screens/all-users.tsx
+++ src/screens/all-users.tsx
@@ -1,11 +1,27 @@
+import useAsync from 'hooks/use-async';
+import { getAllUsers } from 'api';
+import { UserList } from 'types';
+
 export default function AllUsers() {
+  const { status, value: users, error } = useAsync<UserList>(getAllUsers);
+
   return (
     <section>
-      <h2>Get all users</h2>
-      <pre>GET /api/exercise/users</pre>
-      <form action="/api/exercise/users" method="GET">
-        <button type="submit">Submit</button>
-      </form>
+      <h2>All users</h2>
+      {status === 'error' && (
+        <div style={{ backgroundColor: 'red', color: 'white' }}>
+          <p>Error: problem</p>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </div>
+      )}
+      {status === 'pending' && <h2>Loading</h2>}
+
+      <p>There are {users?.length || 0} users</p>
+      <ul>
+        {users?.map(({ _id, username }) => (
+          <li key={_id}>{username} --- {_id}</li>
+        ))}
+      </ul>
     </section>
   );
 }
