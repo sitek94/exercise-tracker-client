cdf007292293d9b707c2a4043854ed46173ca6aa
sitek94 <msitkowski94@gmail.com>
2021-02-18 21:21:43 +0100

    remove old styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeProvider from 'theme';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
