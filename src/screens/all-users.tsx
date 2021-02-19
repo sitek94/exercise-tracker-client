12bef3a70b8679e36ae785f4403a51e58fa2e510
sitek94 <msitkowski94@gmail.com>
2021-02-19 21:27:30 +0100

    wip: styling and layout

--- src/screens/all-users.tsx
+++ src/screens/all-users.tsx
@@ -1,27 +1,46 @@
-import useAsync from 'hooks/use-async';
+import { useAsync } from 'hooks/use-async';
 import { getAllUsers } from 'api';
 import { UserList } from 'types';
+import { Avatar, Box, Container, Paper } from '@material-ui/core';
+import { Title } from 'components/title';
+import { Groups } from '@material-ui/icons';
+import { UsersTable } from 'components/users-table';
 
 export default function AllUsers() {
   const { status, value: users, error } = useAsync<UserList>(getAllUsers);
 
   return (
-    <section>
-      <h2>All users</h2>
-      {status === 'error' && (
-        <div style={{ backgroundColor: 'red', color: 'white' }}>
-          <p>Error: problem</p>
-          <pre>{JSON.stringify(error, null, 2)}</pre>
-        </div>
-      )}
-      {status === 'pending' && <h2>Loading</h2>}
+    <Container maxWidth="sm">
+      <Header />
+      <Paper sx={{ px: 5, py: 5 }}>
+        {status === 'error' && (
+          <div style={{ backgroundColor: 'red', color: 'white' }}>
+            <p>Error: problem</p>
+            <pre>{JSON.stringify(error, null, 2)}</pre>
+          </div>
+        )}
+        {status === 'pending' && <h2>Loading</h2>}
 
-      <p>There are {users?.length || 0} users</p>
-      <ul>
-        {users?.map(({ _id, username }) => (
-          <li key={_id}>{username} --- {_id}</li>
-        ))}
-      </ul>
-    </section>
+        {users ? <UsersTable rows={users} /> : null}
+      </Paper>
+    </Container>
+  );
+}
+
+function Header() {
+  return (
+    <Box
+      sx={{
+        my: 5,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ my: 2, height: 70, width: 70, bgcolor: 'primary.light' }}>
+        <Groups sx={{ fontSize: 40 }} />
+      </Avatar>
+      <Title>All users</Title>
+    </Box>
   );
 }
--- src/screens/new-user.tsx
+++ src/screens/new-user.tsx
@@ -1,59 +1,116 @@
 import * as React from 'react';
-import useAsync from 'hooks/use-async';
+import { useAsync } from 'hooks/use-async';
+import {
+  Alert,
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from '@material-ui/core';
+import { Person } from '@material-ui/icons';
+import { LoadingButton } from '@material-ui/lab';
 import { postNewUser } from 'api';
 import { AxiosError } from 'axios';
+import { ScreenHeader } from 'components/screen-header';
 import { User } from 'types';
-import { Title } from 'components/title';
-import { Alert, TextField } from '@material-ui/core';
-import { LoadingButton } from '@material-ui/lab';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { SingleUserTable } from 'components/single-user-table';
+
+interface FormErrors {
+  username: string;
+}
 
 export default function NewUser() {
   const [username, setUsername] = React.useState('');
 
-  const { status, value, error, execute, reset } = useAsync<User, AxiosError>(
-    async () => postNewUser(username),
-    false,
-  );
+  const { status, value: user, error, execute, reset } = useAsync<
+    User,
+    AxiosError
+  >(async () => postNewUser(username), false);
+  const [formErrors, setFormErrors] = React.useState<FormErrors | null>(null);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (username) {
+    if (!username) {
+      setFormErrors({
+        username: 'Username is required',
+      });
+    } else {
+      setFormErrors(null);
       execute();
     }
   };
 
+  const isError = status === 'error';
+  const isLoading = status === 'pending';
+  const isSuccess = status === 'success';
+
   return (
-    <>
-      <Title>Create New User</Title>
-      {status === 'error' && (
-        <Alert variant="filled" severity="warning">
-          Oppps:
-          {error?.response?.data?.error || error?.message}
-        </Alert>
-      )}
-      {status === 'success' && (
-        <Alert variant="filled" severity="success">
-          The user has been created successfully!
-        </Alert>
-      )}
-      <form onSubmit={handleSubmit}>
-        <TextField
-          id="username"
-          placeholder="johny_bravo_2000"
-          value={username}
-          onChange={e => setUsername(e.currentTarget.value)}
-          fullWidth
-          sx={{ mb: 2 }}
-        />
-        <LoadingButton
-          variant="contained"
-          type="submit"
-          pending={status === 'pending'}
-        >
-          Submit
-        </LoadingButton>
-      </form>
-    </>
+    <Container maxWidth="xs" sx={{ mt: 5 }}>
+      <ScreenHeader title="New user" icon={<Person />} />
+
+      <Box sx={{ my: 3 }}>
+        {isError && (
+          <Alert variant="filled" severity="error">
+            {error?.response?.data?.error || error?.message}
+          </Alert>
+        )}
+
+        {isSuccess && (
+          <Alert variant="filled" severity="success">
+            The user has been created successfully!
+          </Alert>
+        )}
+      </Box>
+
+      <Box>
+        <Typography sx={{ mb: 2 }}></Typography>
+
+        {isSuccess && user ? (
+          <div>
+            <Typography variant="body1" gutterBottom>
+              You can find the newly created user's details below. Remember to
+              save your ID - you will need it to add exercises.
+            </Typography>
+
+            <SingleUserTable user={user} />
+
+            <CopyToClipboard text={user._id}>
+              <Button variant="contained" fullWidth>
+                Copy my user ID
+              </Button>
+            </CopyToClipboard>
+
+            <Button variant="outlined" onClick={reset} fullWidth>
+              Add another user
+            </Button>
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="username"
+              label="Username"
+              placeholder="johny_bravo_2000"
+              value={username}
+              onChange={e => setUsername(e.currentTarget.value)}
+              error={!!formErrors?.username}
+              helperText={formErrors?.username}
+              fullWidth
+              sx={{ mb: 2 }}
+            />
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              pending={isLoading}
+              fullWidth
+            >
+              {isLoading ? 'Loading...' : 'Submit'}
+            </LoadingButton>
+          </form>
+        )}
+      </Box>
+    </Container>
   );
 }
